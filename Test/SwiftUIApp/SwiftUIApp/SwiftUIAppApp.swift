30829563f65bca4aac14c3265d510faf1a68c6ea
chernyaevpavel <pawliklist@yandex.ru>
2022-08-16 08:51:54 +0400

    test

---
+++ Test/SwiftUIApp/SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Павел Черняев on 28.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
